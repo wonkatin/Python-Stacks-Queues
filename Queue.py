d5cdd7dea6dfd8a9e44b5ff0e74bbcd06798585f
h64 <hhongnr@gmail.com>
2019-09-27 10:22:30 -0700

    initial commit

---
+++ Queue.py
@@ -0,0 +1,45 @@
+class Queue:
+    def __init__(self):
+        pass
+
+    def isEmpty(self):
+        """Returns True if Queue is empty, False otherwise"""
+        pass
+
+    def enqueue(self, item):
+        """Adds an item to the back of queue"""
+        pass
+
+    def dequeue(self):
+        """Removes the item from the front of queue. Returns the removed item"""
+        pass
+
+    def peek(self):
+        """Returns the first item in the queue, but doesn't remove it"""
+        pass
+
+    def size(self):
+        """Returns the (int) size of the queue"""
+        pass
+
+    def __str__(self):
+        """Returns a string representation of all items in queue"""
+        return ""
+
+
+my_queue = Queue()
+
+print('Is Queue empty?', my_queue.isEmpty())
+
+my_queue.enqueue('A')
+my_queue.enqueue('B')
+my_queue.enqueue('C')
+my_queue.enqueue('D')
+
+print('Dequeued item:', my_queue.dequeue())
+print('First item:', my_queue.peek())
+
+print('Here are all the items in the queue:', my_queue)
+print('The size of my stack is:', my_queue.size())
+
+# BONUS - Implement the Queue with a Linked List
---
+++ Stack.py
@@ -0,0 +1,45 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        """Returns True if Stack is empty, False otherwise"""
+        pass
+
+    def push(self, item):
+        """Adds an item to the top of stack"""
+        pass
+
+    def pop(self):
+        """Removes the item from the top of stack. Returns the removed item"""
+        pass
+
+    def peek(self):
+        """Returns the top item in the stack, but doesn't remove it"""
+        pass
+
+    def size(self):
+        """Returns the (int) size of the stack"""
+        pass
+
+    def __str__(self):
+        """Returns a string representation of all items in stack"""
+        return ""
+
+
+my_stack = Stack()
+
+print('Is Stack empty?', my_stack.isEmpty())
+
+my_stack.push('A')
+my_stack.push('B')
+my_stack.push('C')
+my_stack.push('D')
+
+print('Popped item:', my_stack.pop())
+print('Top item:', my_stack.peek())
+
+print('Here are all the items in the stack:', my_stack)
+print('The size of my stack is:', my_stack.size())
+
+# BONUS - Implement the Stack with a Linked List
